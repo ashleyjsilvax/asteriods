874bb4f627eed4a3f2a3f37b9ea5ccec1699ccb9
ashleyjsilvax <ashleyjsilvax@gmail.com>
2024-11-20 16:50:31 -0500

    Created main, constants, and gitignore files

---
+++ asteriods/main.py
@@ -0,0 +1,23 @@
+import pygame
+from constants import *
+
+
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    black = (0, 0, 0)  # RGB color for black
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        
+        
+        screen.fill(black)
+        pygame.display.flip()
+        
+    
+
+if __name__ == "__main__":
+    main()
